b002e45b1dcad22fcefb9b7cca7f02e4f5b7f4ff
Mdev122 <cimmerian422@gmail.com>
2026-06-29 20:57:30 +0200

    little update

--- names.js
+++ names.js
@@ -1,16 +1,17 @@
-// Import modułu do pobierania danych od użytkownika w node
-const readlineSync = require('readline-sync');
+let firstName = "Grzegorz";
+console.log(firstName);
+firstName = "Wielki";
+console.log(firstName);
 
-// Etap 1: pobranie wartości przyprostokątnych od użytkownika
-let przyprostokątna1 = readlineSync.question("Podaj długość przyprostokątnej a: ");
-let przyprostokątna2 = readlineSync.question("Podaj długość przyprostokątnej b: ");
 
-// Etap 2: konwersja na liczby (readline-sync, podobnie jak prompt, zwraca tekst!)
-przyprostokątna1 = Number(przyprostokątna1);
-przyprostokątna2 = Number(przyprostokątna2);
+const Age = 29;
+Age = 20;
 
-// Etap 3: obliczenie przeciwprostokątnej ze wzoru Pitagorasa (a² + b² = c²  →  c = √(a² + b²))
-let przeciwprostokątna = Math.sqrt(przyprostokątna1 ** 2 + przyprostokątna2 ** 2);
 
-// Etap 4: wyświetlenie wyniku
-console.log(`Przeciwprostokątna wynosi: ${przeciwprostokątna}`);
+if{
+    let x = 100
+    var y =200;
+}
+console.log(x);
+console.log(y);
+
