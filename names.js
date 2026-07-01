b3ad89c7ce98e7d60b28d77e7e67468a0f2a2edf
Mdev122 <cimmerian422@gmail.com>
2026-07-01 10:03:45 +0200

    Cwiczenie 01: let vs const

--- names.js
+++ names.js
@@ -1,3 +1,14 @@
+//*Cwiczenie pierwsze *//
+
+let temperatura = 20;
+temperatura = 25;
+console.log(temperatura);
+
+const upał = 40;
+upał = 42;
+console.log(upał);
+
+- [x] 01. Zadeklarować tę samą zmienną przez `let` i przez `const`, porównać błędy przy próbie redeklaracji/zmiany wartości
 
 
 
